b288111ea87f7fa546dcf47bee73eb3581baf577
ohernandovisa <111998643+ohernandovisa@users.noreply.github.com>
2024-10-31 11:18:27 -0500

    Delete test directory

--- test/mocks/models/address.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var proxyquire = require('proxyquire').noCallThru().noPreserveCache();
-
-function proxyModel() {
-    return proxyquire('../../../../storefront-reference-architecture/cartridges/app_storefront_base/cartridge/models/address.js', {});
-}
-
-module.exports = proxyModel();
